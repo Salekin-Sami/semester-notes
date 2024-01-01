c8ddd4d358dec9eb17c63d11f8ee86b581af9aca
comfortablynumb13 <sirajussalekin23522@gmail.com>
2024-01-01 18:37:31 +0600

    Redesigned the website

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,3 @@
-const myImage = document.querySelector("img");
-
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "../images/1920px-Gsm_structures.svg.png") {
-    myImage.setAttribute("src", "../images/Cellular_network_standards_and_generation_timeline.svg.png");
-  } else {
-    myImage.setAttribute("src", "../images/1920px-Gsm_structures.svg.png");
-  }
-};
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
@@ -20,6 +10,7 @@ function setUserName() {
     myHeading.textContent = `Welcome to the world of Wireless Communication, ${myName}`;
   }
 }
+
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
