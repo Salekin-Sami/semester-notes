a680df12f8214935ec5b44522d5c27372f032425
comfortablynumb13 <sirajussalekin23522@gmail.com>
2024-01-02 19:55:16 +0600

    Added new page cellular Communication

--- scripts/main.js
+++ scripts/main.js
@@ -21,3 +21,33 @@ if (!localStorage.getItem("name")) {
 myButton.onclick = () => {
   setUserName();
 };
+
+function showLoadingScreen() {
+  document.getElementById('loading-screen').classList.add('active');
+}
+
+function hideLoadingScreen() {
+  document.getElementById('loading-screen').classList.remove('active');
+}
+
+function loadPage(page) {
+  showLoadingScreen();
+
+  document.body.style.backgroundColor = '#f0f0f0'; // Adjust background color during transition
+  document.getElementById('content').style.opacity = '0'; // Fade out the content
+
+  fetch(page)
+      .then(response => response.text())
+      .then(html => {
+          setTimeout(() => {
+              document.body.style.backgroundColor = ''; // Reset background color
+              document.getElementById('content').innerHTML = html;
+              document.getElementById('content').style.opacity = '1'; // Fade in the new content
+              hideLoadingScreen();
+          }, 500); // Adjust the timeout to match the transition duration
+      })
+      .catch(error => {
+          console.error('Error:', error);
+          hideLoadingScreen(); // Ensure loading screen is hidden on error
+      });
+}
